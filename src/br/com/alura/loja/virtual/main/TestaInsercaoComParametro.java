0cea29f56a30850dd78c1b3806d70f366f2d6d0e
Gean <gean.milhore@gmail.com>
2022-11-06 23:52:48 -0300

    aula 4 - refresh

--- src/br/com/alura/loja/virtual/main/TestaInsercaoComParametro.java
+++ src/br/com/alura/loja/virtual/main/TestaInsercaoComParametro.java
@@ -9,25 +9,39 @@ import java.sql.Statement;
 public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
-		
-		String nome = "Mouse'";
-		String descricao = "Mouse sem fio); delete from Produto; ";
-		
-		Connection connection = ConnectionFactory.recuperarConexao();
-		
-		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?,?)"
-				, Statement.RETURN_GENERATED_KEYS);
+
+		try (Connection connection = ConnectionFactory.recuperarConexao();) {
+
+			connection.setAutoCommit(false);
+			try (PreparedStatement stm = connection.prepareStatement(
+					"INSERT INTO PRODUTO (nome, descricao) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS)) {
+				adicionarVariavel("SmartTv", "45 polegadas", stm);
+				adicionarVariavel("Rádio", "rádio de bateria", stm);
+
+				connection.commit();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("Rollback Executado");
+				connection.rollback();
+			}
+		}
+	}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
-		stm.execute();
-		
-		ResultSet rst = stm.getGeneratedKeys();
-		
-		while(rst.next()) {
-			Integer id = rst.getInt(1);
-			System.out.println("O id criado foi "+ id);
+		if (nome.equals("Rádio")) {
+			throw new RuntimeException("Não foi possível adicionar o seu produto");
 		}
 
+		stm.execute();
+
+		try (ResultSet rst = stm.getGeneratedKeys()) {
+			while (rst.next()) {
+				Integer id = rst.getInt(1);
+				System.out.println("O id criado foi " + id);
+			}
+		}
 	}
 
 }
--- src/br/com/alura/loja/virtual/main/TestaRemocao.java
+++ src/br/com/alura/loja/virtual/main/TestaRemocao.java
@@ -3,7 +3,6 @@ package br.com.alura.loja.virtual.main;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class TestaRemocao {
 
